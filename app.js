776652c1192446fc39470ed05ee48100f548a30a
TiagoFerlaCamini <tiagoferla1212@gmail.com>
2024-08-01 16:17:02 -0300

    PRIMERO COMIT

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+  this.classList.toggle('active');
+  //document.getElementById('mobile-menu').classList.toggle('active');
+});
