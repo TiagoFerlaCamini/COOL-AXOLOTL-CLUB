0b76af5e22269ac8cb616c6d22a0eb8b8df5fd99
TiagoFerlaCamini <tiagoferla1212@gmail.com>
2024-08-05 18:40:24 -0300

    s

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-  this.classList.toggle('active');
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  this.classList.toggle("active");
   //document.getElementById('mobile-menu').classList.toggle('active');
 });
+
+
+/*/modo dark da pagina */
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", function () {
+  const main = document.querySelector("main");
+  main.style.backgroundColor = '#261C24';
+
+  const header = document.querySelector("header");
+  header.style.background = '#261C24'
+  return;
+});
+/* fim do modo escuro da pagina */
+
+const btn2 = document.querySelector('#btn2')
+btn2.addEventListener('click',function(){
+  const main = document.querySelector("main");
+  main.style.backgroundColor = '#5e17eb';
+
+  const header = document.querySelector("header");
+  header.style.background = '#5e17eb'
+})
